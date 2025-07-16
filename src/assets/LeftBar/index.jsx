459f8f7a6f283434590cb7177ddc06fa9d61c696
AlfaCefOf <alfaxcef@gmail.com>
2025-07-16 17:43:52 +0500

    edited

--- src/assets/LeftBar/index.jsx
+++ src/assets/LeftBar/index.jsx
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import filterLogo from "../img/filterLogo.png"
-import arrowBot from "../img/arrowBottom.png"
+import filterLogo from "../img/filterLogo.png";
+import arrowBot from "../img/arrowBottom.png";
 
 function LeftBar() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const filters = ["Status", "Price", "Collections", "Chains", "Categories"];
+
+  const handleClick = (index) => {
+    setOpenIndex(openIndex === index ? null : index); // закрывает, если уже открыт
+  };
+
   return (
     <div className="leftbar">
       <div className="filter-header">
-        <img src={filterLogo} alt="" />
+        <img src={filterLogo} alt="Filter" />
       </div>
       <div className="filter-group">
-        <div className="filter-item">
-        <p>Status</p> <img src={arrowBot} alt="" />
-        </div>
-        <div className="filter-item">
-          <p>Price</p> <img src={arrowBot} alt="" />
-        </div>
-        <div className="filter-item">
-          <p>Collections</p> <img src={arrowBot} alt="" />
-        </div>
-        <div className="filter-item">
-          <p>Chains</p> <img src={arrowBot} alt="" />
-        </div>
-        <div className="filter-item">
-          <p>Categories</p> <img src={arrowBot} alt="" />
-        </div>
+        {filters.map((filter, index) => (
+          <div key={filter}>
+            <div
+              className={`filter-item ${openIndex === index ? "active" : ""}`}
+              onClick={() => handleClick(index)}
+            >
+              <p>{filter}</p>
+              <img src={arrowBot} alt="Arrow" />
+            </div>
+            {openIndex === index && (
+              <div className="dropdown-content">
+                <p>{filter}</p>
+                <p>{filter}</p>
+                <p>{filter}</p>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
